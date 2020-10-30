54dc2d8b5dc2f9f8512fb1fec409bb44766dc15f
Gus Narea <gnarea@users.noreply.github.com>
2020-10-30 23:08:40 +0000

    fix: Add cloudresourcemanager dependencies

--- tf-workspace/gcp.tf
+++ tf-workspace/gcp.tf
@@ -8,10 +8,14 @@ resource "google_project_service" "logging" {
   project                    = var.gcp_project_id
   service                    = "logging.googleapis.com"
   disable_dependent_services = true
+
+  depends_on = [google_project_service.cloudresourcemanager]
 }
 
 resource "google_project_service" "compute" {
   project                    = var.gcp_project_id
   service                    = "compute.googleapis.com"
   disable_dependent_services = true
+
+  depends_on = [google_project_service.cloudresourcemanager]
 }
