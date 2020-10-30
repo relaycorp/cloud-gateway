54aea421a07fe211fe13b47c83d0071fcf59e1d9
Gus Narea <gnarea@users.noreply.github.com>
2020-10-30 22:32:42 +0000

    chore: Enable compute.googleapis.com

--- tf-workspace/gcp.tf
+++ tf-workspace/gcp.tf
@@ -3,3 +3,9 @@ resource "google_project_service" "logging" {
   service                    = "logging.googleapis.com"
   disable_dependent_services = true
 }
+
+resource "google_project_service" "compute" {
+  project                    = var.gcp_project_id
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = true
+}
