5c92f085c9cee02a102c106ab6bd1e07273e5c15
Gus Narea <gnarea@users.noreply.github.com>
2020-11-01 13:02:52 +0000

    Revert "Destroy test env"
    This reverts commit e7bcc16f

--- tf-workspace/env-test.tf
+++ tf-workspace/env-test.tf
@@ -1,18 +1,18 @@
-//module "gw_test" {
-//  source = "../tf-modules/base"
-//
-//  environment_name = "test"
-//
-//  pohttp_host = "pohttp-test.relaycorp.tech"
-//  poweb_host  = "poweb-test.relaycorp.tech"
-//  cogrpc_host = "cogrpc-test.relaycorp.tech"
-//
-//  gcp_project_id           = var.gcp_project_id
-//  mongodb_atlas_project_id = var.mongodb_atlas_project_id
-//
-//  depends_on = [
-//    google_project_service.logging,
-//    google_project_service.compute,
-//    google_project_service.container,
-//  ]
-//}
+module "gw_test" {
+  source = "../tf-modules/base"
+
+  environment_name = "test"
+
+  pohttp_host = "pohttp-test.relaycorp.tech"
+  poweb_host  = "poweb-test.relaycorp.tech"
+  cogrpc_host = "cogrpc-test.relaycorp.tech"
+
+  gcp_project_id           = var.gcp_project_id
+  mongodb_atlas_project_id = var.mongodb_atlas_project_id
+
+  depends_on = [
+    google_project_service.logging,
+    google_project_service.compute,
+    google_project_service.container,
+  ]
+}
