33a3a402a8a99929cfa3a53b5355888377ec1745
Gus Narea <gnarea@users.noreply.github.com>
2020-11-09 22:03:02 +0000

    Revert "temporarily destroy Vault pipeline"
    This reverts commit 25cda3f2

--- tf-modules/cd/vault.tf
+++ tf-modules/cd/vault.tf
@@ -1,49 +1,49 @@
-//resource "codefresh_pipeline" "test" {
-//  lifecycle {
-//    ignore_changes = [
-//      revision,
-//
-//      // See: https://github.com/codefresh-io/terraform-provider-codefresh/issues/21
-//      project_id
-//    ]
-//  }
-//
-//  name = "${var.cf_project_name}/vault-${var.environment_name}"
-//
-//  tags = [
-//    "gateway",
-//    "env-${var.environment_name}",
-//    "service-vault"
-//  ]
-//
-//  spec {
-//    concurrency = 1
-//    priority    = 5
-//
-//    trigger {
-//      context     = "git"
-//      description = "Trigger for commits"
-//      disabled    = false
-//      events = [
-//        "push.heads"
-//      ]
-//      modified_files_glob = "**/vault.yml"
-//      name                = "commits"
-//      provider            = "github"
-//      repo                = "relaycorp/cloud-gateway"
-//      branch_regex        = "/^master$/"
-//      type                = "git"
-//    }
-//
-//    variables = {
-//      KUBERNETES_CONTEXT = var.cf_kubernetes_context
-//    }
-//
-//    spec_template {
-//      repo     = "relaycorp/cloud-gateway"
-//      path     = "./cf-pipelines/vault.yml"
-//      revision = "master"
-//      context  = "git"
-//    }
-//  }
-//}
+resource "codefresh_pipeline" "test" {
+  lifecycle {
+    ignore_changes = [
+      revision,
+
+      // See: https://github.com/codefresh-io/terraform-provider-codefresh/issues/21
+      project_id
+    ]
+  }
+
+  name = "${var.cf_project_name}/vault-${var.environment_name}"
+
+  tags = [
+    "gateway",
+    "env-${var.environment_name}",
+    "service-vault"
+  ]
+
+  spec {
+    concurrency = 1
+    priority    = 5
+
+    trigger {
+      context     = "git"
+      description = "Trigger for commits"
+      disabled    = false
+      events = [
+        "push.heads"
+      ]
+      modified_files_glob = "**/vault.yml"
+      name                = "commits"
+      provider            = "github"
+      repo                = "relaycorp/cloud-gateway"
+      branch_regex        = "/^master$/"
+      type                = "git"
+    }
+
+    variables = {
+      KUBERNETES_CONTEXT = var.cf_kubernetes_context
+    }
+
+    spec_template {
+      repo     = "relaycorp/cloud-gateway"
+      path     = "./cf-pipelines/vault.yml"
+      revision = "master"
+      context  = "git"
+    }
+  }
+}
