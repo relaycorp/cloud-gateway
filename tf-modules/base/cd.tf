0bda6d0ca03cc0ddfb601fae9737361804e31195
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 22:21:01 +0000

    Revert "Temporarily destroy k8s provier"
    This reverts commit 4cda0484

--- tf-modules/base/cd.tf
+++ tf-modules/base/cd.tf
@@ -1,13 +1,12 @@
 # Retrieve an access token as the Terraform runner
 data "google_client_config" "provider" {}
 
-// TODO: REMOVE
-//provider "kubernetes" {
-//  load_config_file = false
-//
-//  host  = "https://${google_container_cluster.primary.endpoint}"
-//  token = data.google_client_config.provider.access_token
-//  cluster_ca_certificate = base64decode(
-//    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
-//  )
-//}
+provider "kubernetes" {
+  load_config_file = false
+
+  host  = "https://${google_container_cluster.main.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.main.master_auth[0].cluster_ca_certificate,
+  )
+}
