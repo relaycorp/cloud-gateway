0815b17087e21e43943d7483599ecf2c88599a82
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 19:16:25 +0000

    Move GKE cluster admin SA to separate module

--- tf-modules/gke-admin-access/providers.tf
+++ tf-modules/gke-admin-access/providers.tf
@@ -1,13 +1,10 @@
 # Retrieve an access token as the Terraform runner
 data "google_client_config" "provider" {}
 
-// TODO: REMOVE
 provider "kubernetes" {
   load_config_file = false
 
-  host  = "https://${google_container_cluster.primary.endpoint}"
+  host  = "https://${var.gke_cluster_endpoint}"
   token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
-  )
+  cluster_ca_certificate = base64decode(var.gke_cluster_ca_certificate)
 }
