a611044576f31fa69d8f81b984eed6fba9b613fc
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 18:43:54 +0000

    Destroy test environment to fix various issues with cluster

--- tf-workspace/env-test.tf
+++ tf-workspace/env-test.tf
@@ -1,17 +1,17 @@
-module "gw_test" {
-  source = "../tf-modules/base"
-
-  environment_name = "test"
-
-  pohttp_host = "pohttp-test.relaycorp.tech"
-  poweb_host  = "poweb-test.relaycorp.tech"
-  cogrpc_host = "cogrpc-test.relaycorp.tech"
-
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = "europe-west2"
-
-  mongodb_atlas_project_id = var.mongodb_atlas_project_id
-
-  cf_kubernetes_context = "gateway-test"
-  cf_project_name       = codefresh_project.gateway.name
-}
+//module "gw_test" {
+//  source = "../tf-modules/base"
+//
+//  environment_name = "test"
+//
+//  pohttp_host = "pohttp-test.relaycorp.tech"
+//  poweb_host  = "poweb-test.relaycorp.tech"
+//  cogrpc_host = "cogrpc-test.relaycorp.tech"
+//
+//  gcp_project_id = var.gcp_project_id
+//  gcp_region     = "europe-west2"
+//
+//  mongodb_atlas_project_id = var.mongodb_atlas_project_id
+//
+//  cf_kubernetes_context = "gateway-test"
+//  cf_project_name       = codefresh_project.gateway.name
+//}
