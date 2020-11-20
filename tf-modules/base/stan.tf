ad162d1a85057b17035f9609e11d8fe1bf2915ae
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 10:21:47 +0000

    NATS Streaming: Combine PostgreSQL users

--- tf-modules/base/stan.tf
+++ tf-modules/base/stan.tf
@@ -3,23 +3,13 @@ resource "google_sql_database" "postgresql_stan" {
   instance = google_sql_database_instance.postgresql.name
 }
 
-resource "google_sql_user" "postgresql_stan_runtime" {
+resource "google_sql_user" "postgresql_stan" {
   name     = "stan"
   instance = google_sql_database_instance.postgresql.name
-  password = random_password.postgresql_stan_runtime.result
+  password = random_password.postgresql_stan.result
 }
 // TODO: Use service accounts instead (https://github.com/relaycorp/cloud-gateway/issues/6)
-resource "random_password" "postgresql_stan_runtime" {
-  length = 32
-}
-
-resource "google_sql_user" "postgresql_stan_initdb" {
-  name     = "stan-initdb"
-  instance = google_sql_database_instance.postgresql.name
-  password = random_password.postgresql_stan_initdb.result
-}
-// TODO: Use service accounts instead (https://github.com/relaycorp/cloud-gateway/issues/6)
-resource "random_password" "postgresql_stan_initdb" {
+resource "random_password" "postgresql_stan" {
   length = 32
 }
 
@@ -29,7 +19,6 @@ resource "kubernetes_secret" "stan_cd" {
   }
 
   data = {
-    postgresql_runtime_user_password = google_sql_user.postgresql_stan_runtime.password
-    postgresql_initdb_user_password  = google_sql_user.postgresql_stan_initdb.password
+    postgresql_user_password = google_sql_user.postgresql_stan.password
   }
 }
