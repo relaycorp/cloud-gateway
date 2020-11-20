61f7731d2d20dd8e1eaf276d4e838611def5f9f7
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 18:32:05 +0000

    GKE: Deploy cluster to custom network instead of default one

--- tf-modules/base/cd.tf
+++ tf-modules/base/cd.tf
@@ -9,10 +9,10 @@ locals {
 provider "kubernetes" {
   load_config_file = false
 
-  host  = "https://${google_container_cluster.primary.endpoint}"
+  host  = "https://${google_container_cluster.main.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.main.master_auth[0].cluster_ca_certificate,
   )
 }
 
--- tf-modules/base/outputs.tf
+++ tf-modules/base/outputs.tf
@@ -90,10 +90,10 @@ EOF
 }
 
 output "gke_cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
+  value = google_container_cluster.main.endpoint
 }
 output "gke_cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  value = google_container_cluster.main.master_auth[0].cluster_ca_certificate
 }
 output "cd_service_account_secret" {
   value     = data.kubernetes_secret.cd_service_access.data.token
