4cda04843a37818a0a72f9e823f279b95e8f852d
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 19:47:20 +0000

    Temporarily destroy k8s provier

--- tf-modules/base/cd.tf
+++ tf-modules/base/cd.tf
@@ -2,12 +2,12 @@
 data "google_client_config" "provider" {}
 
 // TODO: REMOVE
-provider "kubernetes" {
-  load_config_file = false
-
-  host  = "https://${google_container_cluster.primary.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
-  )
-}
+//provider "kubernetes" {
+//  load_config_file = false
+//
+//  host  = "https://${google_container_cluster.primary.endpoint}"
+//  token = data.google_client_config.provider.access_token
+//  cluster_ca_certificate = base64decode(
+//    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+//  )
+//}
