1a21625f7812a73d0ed8a24ee620d0341a48ab1e
Gus Narea <gnarea@users.noreply.github.com>
2020-11-20 13:19:00 +0000

    NATS: Set up CD

--- tf-modules/base/stan.tf
+++ tf-modules/base/stan.tf
@@ -22,3 +22,56 @@ resource "kubernetes_secret" "stan_cd" {
     postgresql_user_password = google_sql_user.postgresql_stan.password
   }
 }
+
+// Continuous Deployment
+
+resource "codefresh_pipeline" "nats" {
+  lifecycle {
+    ignore_changes = [
+      revision,
+
+      // See: https://github.com/codefresh-io/terraform-provider-codefresh/issues/21
+      project_id
+    ]
+  }
+
+  name = "${var.cf_project_name}/${var.environment_name}: NATS"
+
+  tags = [
+    "gateway",
+    "env-${var.environment_name}",
+    "service-nats"
+  ]
+
+  spec {
+    concurrency = 1
+    priority    = 5
+
+    trigger {
+      context     = "github"
+      description = "Trigger for commits"
+      disabled    = false
+      events = [
+        "push.heads"
+      ]
+      modified_files_glob = "**/nats.yml"
+      name                = "commits"
+      provider            = "github"
+      repo                = "relaycorp/cloud-gateway"
+      branch_regex        = "/^main$/"
+      type                = "git"
+    }
+
+    variables = {
+      KUBERNETES_CONTEXT = var.cf_kubernetes_context
+      VAULT_KV_PREFIX    = local.vault_kv_prefix
+    }
+
+    spec_template {
+      repo     = "relaycorp/cloud-gateway"
+      path     = "./cf-pipelines/nats.yml"
+      revision = "main"
+      context  = "github"
+    }
+  }
+}
