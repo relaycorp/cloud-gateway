9dbdb62ea03da36b51b4f63bdc940363cb4b3dcb
Gus Narea <gnarea@users.noreply.github.com>
2020-11-21 13:05:56 +0000

    chore: Fix formatting

--- tf-modules/gke-admin-access/providers.tf
+++ tf-modules/gke-admin-access/providers.tf
@@ -4,7 +4,7 @@ data "google_client_config" "provider" {}
 provider "kubernetes" {
   load_config_file = false
 
-  host  = "https://${var.gke_cluster_endpoint}"
-  token = data.google_client_config.provider.access_token
+  host                   = "https://${var.gke_cluster_endpoint}"
+  token                  = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(var.gke_cluster_ca_certificate)
 }
