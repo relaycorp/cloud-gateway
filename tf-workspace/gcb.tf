1b8ec619a0104d4aef92112aa9fdd68d675ee49d
Gus Narea <gnarea@users.noreply.github.com>
2020-11-22 15:50:19 +0000

    GCB: Use non-authoritative IAM binding to avoid clashes

--- tf-workspace/gcb.tf
+++ tf-workspace/gcb.tf
@@ -2,7 +2,7 @@ locals {
   gcb_service_account_email = "${data.google_project.main.number}@cloudbuild.gserviceaccount.com"
 }
 
-resource "google_project_iam_binding" "gcb_gke_developer" {
-  role    = "roles/container.developer"
-  members = ["serviceAccount:${local.gcb_service_account_email}"]
+resource "google_project_iam_member" "gcb_gke_developer" {
+  role   = "roles/container.developer"
+  member = "serviceAccount:${local.gcb_service_account_email}"
 }
