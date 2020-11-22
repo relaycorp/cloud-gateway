a5c9997cbd437d93de5577b8f9ee001838877858
Gus Narea <gnarea@users.noreply.github.com>
2020-11-22 15:34:16 +0000

    GCB: Add name and description to trigger

--- tf-modules/base/cd_kubernetes.tf
+++ tf-modules/base/cd_kubernetes.tf
@@ -1,4 +1,7 @@
 resource "google_cloudbuild_trigger" "main" {
+  name        = "${var.environment_name}-k8s-deployment"
+  description = "Deploy and configure Kubernetes resources in environment ${var.environment_name}"
+
   github {
     owner = var.github_repo.organisation
     name  = var.github_repo.name
