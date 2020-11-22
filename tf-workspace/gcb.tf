814c3a4d76a654c59750d22c784d87cea4decdc4
Gus Narea <gnarea@users.noreply.github.com>
2020-11-22 17:57:27 +0000

    GCB community builders: Fix typo

--- tf-workspace/gcb.tf
+++ tf-workspace/gcb.tf
@@ -33,7 +33,7 @@ resource "google_cloudbuild_trigger" "gcb_builder_helmfile" {
     step {
       name = "gcr.io/cloud-builders/git"
       id   = "checkout"
-      args = ["$${GIT_REVISION}"]
+      args = ["checkout", "$${GIT_REVISION}"]
 
       wait_for = ["clone"]
     }
