65190b0a775dfe93d38614be67e427307f8ce95a
Gus Narea <gnarea@users.noreply.github.com>
2020-11-26 11:05:37 +0000

    Revert "GCB: Grant container.mutatingWebhookConfigurations.*"
    This reverts commit 95443df5

--- tf-modules/base/gke.tf
+++ tf-modules/base/gke.tf
@@ -61,7 +61,8 @@ resource "google_project_iam_custom_role" "gke_limited_admin" {
   role_id = "${replace(local.env_full_name, "-", "_")}.gke_limited_admin"
   title   = "Limited permissions to manage the GKE cluster"
   permissions = [
-    "container.mutatingWebhookConfigurations.*",
+    "container.mutatingWebhookConfigurations.update",
+    "container.mutatingWebhookConfigurations.delete",
     "container.clusterRoles.create",
     "container.clusterRoles.delete",
     "container.clusterRoleBindings.delete",
