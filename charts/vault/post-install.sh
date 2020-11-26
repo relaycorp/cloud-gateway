c62e8325228649661830eeacfe127351a1c81c1a
Gus Narea <gnarea@users.noreply.github.com>
2020-11-26 17:14:12 +0000

    Vault: Pipe to `cat` to work around TTY permission issue

--- charts/vault/post-install.sh
+++ charts/vault/post-install.sh
@@ -47,12 +47,13 @@ keybase_encrypt() {
   local keybase_username="$1"
 
   local public_key_url="https://keybase.io/${keybase_username}/pgp_keys.asc"
-  gpg --import <(curl --silent "${public_key_url}")
+  gpg --import <(curl --silent "${public_key_url}") >>/dev/null
   # shellcheck disable=SC2155
   local recipient_email_address="$(
     gpg --list-keys | grep --only-matching --extended-regexp '([^@<]+@[^>]+)'
   )"
-  gpg --recipient "${recipient_email_address}" --encrypt --armor
+  # Pipe to `cat` to work around TTY permission issue
+  gpg --recipient "${recipient_email_address}" --encrypt --armor | cat
 }
 
 # Main
