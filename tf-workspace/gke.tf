b1e0b2dfc1ab8820c422616211d56720779b70f4
Gus Narea <gnarea@users.noreply.github.com>
2020-11-26 12:16:51 +0000

    GCB: Grant GKE developer role again (in addition to admin)

--- tf-workspace/gke.tf
+++ tf-workspace/gke.tf
@@ -1,5 +1,10 @@
 resource "google_project_iam_binding" "gke_developers" {
+  role    = "roles/container.developer"
+  members = [var.sre_iam_uri, "serviceAccount:${local.gcb_service_account_email}"]
+}
+
+resource "google_project_iam_binding" "gke_admins" {
   # TODO: Use developer role instead. See: https://github.com/relaycorp/cloud-gateway/issues/7
   role    = "roles/container.clusterAdmin"
-  members = [var.sre_iam_uri, "serviceAccount:${local.gcb_service_account_email}"]
+  members = ["serviceAccount:${local.gcb_service_account_email}"]
 }
