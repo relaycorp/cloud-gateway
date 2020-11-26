b7caf902850f74c93ff6c6886049f9b462693a57
Gus Narea <gnarea@users.noreply.github.com>
2020-11-26 12:08:27 +0000

    GCB: Work around Kubernetes RBAC issue in #7

--- tf-workspace/gke.tf
+++ tf-workspace/gke.tf
@@ -1,4 +1,5 @@
 resource "google_project_iam_binding" "gke_developers" {
-  role    = "roles/container.developer"
+  # TODO: Use developer role instead. See: https://github.com/relaycorp/cloud-gateway/issues/7
+  role    = "roles/container.clusterAdmin"
   members = [var.sre_iam_uri, "serviceAccount:${local.gcb_service_account_email}"]
 }
