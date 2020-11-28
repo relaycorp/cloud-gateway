e67741a54cc8829f0d22652c316397fd8105e636
Gus Narea <gnarea@users.noreply.github.com>
2020-11-28 16:54:00 +0000

    DNS: Enable DNSSEC

--- tf-workspace/dns.tf
+++ tf-workspace/dns.tf
@@ -2,4 +2,8 @@ resource "google_dns_managed_zone" "relaycorp_cloud" {
   name        = "relaycorp-cloud"
   dns_name    = "relaycorp.cloud."
   description = "Relaycorp Cloud"
+
+  dnssec_config {
+    state = "on"
+  }
 }
