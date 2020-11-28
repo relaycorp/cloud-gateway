d0a230ce60127e66c7c979af16da1bca1be593ba
Gus Narea <gnarea@users.noreply.github.com>
2020-11-28 13:04:35 +0000

    GCB: Retrieve MongoDB password

--- charts/scripts/retrieve-secrets.sh
+++ charts/scripts/retrieve-secrets.sh
@@ -18,7 +18,7 @@ cd secrets
 retrieve_secret "${VAULT_SA_CREDENTIALS_SECRET_VERSION}" > vault-sa-credentials
 retrieve_secret "${STAN_DB_PASSWORD_SECRET_VERSION}" > stan-db-password
 retrieve_secret "${MINIO_SECRET_KEY_SECRET_VERSION}" > minio-secret-key
-retrieve_secret "${GW_MONGODB_CONNECTION_URI_SECRET_VERSION}" > gw-mongodb-connection-uri
+retrieve_secret "${GW_MONGODB_PASSWORD_SECRET_VERSION}" > gw-mongodb-password
 
 if ! retrieve_secret "${VAULT_ROOT_TOKEN_SECRET_ID}" > vault-root-token; then
   echo "Could not retrieve Vault root token. Vault may not be initialised yet." >&2
