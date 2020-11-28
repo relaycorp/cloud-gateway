b72314e2c4f8d895fed0d64addb9f8fbd26ceb34
Gus Narea <gnarea@users.noreply.github.com>
2020-11-28 13:12:50 +0000

    GCB: Fix retrieval of secrets

--- charts/scripts/retrieve-secrets.sh
+++ charts/scripts/retrieve-secrets.sh
@@ -5,21 +5,27 @@ set -o pipefail
 
 # Constants and functions
 
-retrieve_secret() {
+retrieve_secret_version() {
   local secret_version="$1"
 
   gcloud secrets versions access "${secret_version}"
+}
+
+retrieve_latest_secret_version() {
+  local secret_id="$1"
+
+  gcloud secrets versions access latest --secret="${secret_id}"
 }
 
 # Main
 
 mkdir secrets
 cd secrets
-retrieve_secret "${VAULT_SA_CREDENTIALS_SECRET_VERSION}" > vault-sa-credentials
-retrieve_secret "${STAN_DB_PASSWORD_SECRET_VERSION}" > stan-db-password
-retrieve_secret "${MINIO_SECRET_KEY_SECRET_VERSION}" > minio-secret-key
-retrieve_secret "${GW_MONGODB_PASSWORD_SECRET_VERSION}" > gw-mongodb-password
+retrieve_secret_version "${VAULT_SA_CREDENTIALS_SECRET_VERSION}" >vault-sa-credentials
+retrieve_secret_version "${STAN_DB_PASSWORD_SECRET_VERSION}" >stan-db-password
+retrieve_secret_version "${MINIO_SECRET_KEY_SECRET_VERSION}" >minio-secret-key
+retrieve_secret_version "${GW_MONGODB_PASSWORD_SECRET_VERSION}" >gw-mongodb-password
 
-if ! retrieve_secret "${VAULT_ROOT_TOKEN_SECRET_ID}" > vault-root-token; then
+if ! retrieve_latest_secret_version "${VAULT_ROOT_TOKEN_SECRET_ID}" >vault-root-token; then
   echo "Could not retrieve Vault root token. Vault may not be initialised yet." >&2
 fi
