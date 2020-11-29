d74e11e17f8a6e34996ccdc2419ede96b4f51ffc
Gus Narea <gnarea@users.noreply.github.com>
2020-11-29 19:09:12 +0000

    MongoDB: Broaden VPN CIDR block to include all internal IP addresses

--- tf-workspace/mongodb.tf
+++ tf-workspace/mongodb.tf
@@ -1,5 +1,5 @@
 resource "mongodbatlas_project_ip_whitelist" "gcp_vpc" {
   project_id = var.mongodb_atlas_project_id
-  cidr_block = "10.128.0.0/9" # https://cloud.google.com/vpc/docs/vpc#ip-ranges
+  cidr_block = "10.0.0.0/8"
   comment    = "Allow connections from GCP VPCs"
 }
