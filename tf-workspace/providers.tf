b9926eeb94066505f9b11b48184341a3e12c4441
Gus Narea <gnarea@users.noreply.github.com>
2020-11-30 16:44:17 +0000

    GCP: Remove hardcoded region from provider

--- tf-workspace/providers.tf
+++ tf-workspace/providers.tf
@@ -1,13 +1,9 @@
 provider "google" {
   project = var.gcp_project_id
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
 }
 
 provider "google-beta" {
   project = var.gcp_project_id
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
 }
 
 provider "mongodbatlas" {
