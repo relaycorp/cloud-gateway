43709c80c76b9cff2f3c39449a4f460ff0e0a14c
Gus Narea <gnarea@users.noreply.github.com>
2020-11-30 23:14:20 +0000

    Frankfurt: Init

---
+++ environments/frankfurt/main.tf
@@ -0,0 +1,13 @@
+module "gateway" {
+  source = "../_modules/gateway"
+
+  name = "frankfurt"
+
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = "europe-west3"
+
+  mongodb_atlas_project_id = var.mongodb_atlas_project_id
+  mongodb_atlas_region     = "EUROPE_WEST_3"
+
+  sre_iam_uri = var.sre_iam_uri
+}
---
+++ environments/frankfurt/variables.tf
@@ -0,0 +1,7 @@
+variable "gcp_project_id" {
+  default = "relaycorp-cloud-gateway"
+}
+
+variable "mongodb_atlas_project_id" {}
+
+variable "sre_iam_uri" {}
