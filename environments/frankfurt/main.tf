1cde106b131679318b6429623308ba1d8d226741
Gus Narea <gnarea@users.noreply.github.com>
2020-12-01 17:14:55 +0000

    Reinstate the Frankfurt environment

--- environments/frankfurt/main.tf
+++ environments/frankfurt/main.tf
@@ -8,16 +8,16 @@ terraform {
   }
 }
 
-//module "gateway" {
-//  source = "../_modules/gateway"
-//
-//  name = "frankfurt"
-//
-//  gcp_project_id = var.gcp_project_id
-//  gcp_region     = "europe-west3"
-//
-//  mongodb_atlas_project_id = var.mongodb_atlas_project_id
-//  mongodb_atlas_region     = "EUROPE_WEST_3"
-//
-//  sre_iam_uri = var.sre_iam_uri
-//}
+module "gateway" {
+  source = "../_modules/gateway"
+
+  name = "frankfurt"
+
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = "europe-west3"
+
+  mongodb_atlas_project_id = var.mongodb_atlas_project_id
+  mongodb_atlas_region     = "EUROPE_WEST_3"
+
+  sre_iam_uri = var.sre_iam_uri
+}
