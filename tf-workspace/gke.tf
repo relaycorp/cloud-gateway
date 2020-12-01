2b54fe37c32f837d2704abd7c2ea871946732970
Gus Narea <gnarea@users.noreply.github.com>
2020-12-01 23:17:28 +0000

    Revert "Temporarily grant myself KMS admin role"
    This reverts commit f8aa6f21

--- tf-workspace/gke.tf
+++ tf-workspace/gke.tf
@@ -8,9 +8,3 @@ resource "google_project_iam_binding" "gke_admins" {
   role    = "roles/container.admin"
   members = ["serviceAccount:${local.gcb_service_account_email}"]
 }
-
-# TODO: REMOVE
-resource "google_project_iam_binding" "sre_kms_admin" {
-  role    = "roles/cloudkms.admin"
-  members = [var.sre_iam_uri]
-}
