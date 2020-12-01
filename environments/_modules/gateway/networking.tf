7d73f0b97aae8ee84fbd34f80814bdf3a806b1d6
Gus Narea <gnarea@users.noreply.github.com>
2020-12-01 19:56:58 +0000

    Global address: Add labels

--- environments/_modules/gateway/networking.tf
+++ environments/_modules/gateway/networking.tf
@@ -6,6 +6,8 @@ resource "google_compute_global_address" "managed_tls_cert" {
   name = local.env_full_name
 
   labels = local.gcp_resource_labels
+
+  provider = google-beta
 }
 
 resource "google_compute_firewall" "neg_backend_workaround" {
