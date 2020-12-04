0705a9463cd2040097124984ffd28915f67bb53c
Gus Narea <gnarea@users.noreply.github.com>
2020-12-04 23:32:23 +0000

    Revert "GKE: Allow clusters to be recreated (#39)"
    This reverts commit 3816afa7

--- environments/_modules/gateway/gke.tf
+++ environments/_modules/gateway/gke.tf
@@ -1,9 +1,5 @@
-resource "random_id" "gke_cluster_suffix" {
-  byte_length = 3
-}
-
 resource "google_container_cluster" "main" {
-  name = "${local.env_full_name}-${random_id.gke_cluster_suffix.hex}"
+  name = local.env_full_name
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
