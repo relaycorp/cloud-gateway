2af8df4eb31da10a3a32635d6d9eb911cae0d814
Gus Narea <gnarea@users.noreply.github.com>
2020-12-05 19:11:00 +0000

    Temporarily grant myself compute.instanceAdmin

--- tf-workspace/gke.tf
+++ tf-workspace/gke.tf
@@ -3,6 +3,11 @@ resource "google_project_iam_binding" "gke_developers" {
   members = [var.sre_iam_uri, "serviceAccount:${local.gcb_service_account_email}"]
 }
 
+resource "google_project_iam_binding" "gce_admin" {
+  role    = "roles/compute.instanceAdmin"
+  members = [var.sre_iam_uri]
+}
+
 resource "google_project_iam_binding" "gke_admins" {
   # TODO: Delete. See: https://github.com/relaycorp/cloud-gateway/issues/7
   role    = "roles/container.admin"
