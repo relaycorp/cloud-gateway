ae4e4b2536aab95a2287d2e0b09a2d474e1c1ea9
Gus Narea <gnarea@users.noreply.github.com>
2020-12-06 12:47:08 +0000

    KMS: Fix key ring name

--- environments/_modules/gateway/kms.tf
+++ environments/_modules/gateway/kms.tf
@@ -5,8 +5,8 @@ resource "random_id" "kms_key_ring_suffix" {
 resource "google_kms_key_ring" "main" {
   # Key rings can be deleted from the Terraform state but not GCP, so let's add a suffix in case
   # we need to recreate it.
-  project = "${var.gcp_project_id}-${random_id.kms_key_ring_suffix.hex}"
+  project = var.gcp_project_id
 
-  name     = local.env_full_name
+  name     = "${local.env_full_name}-${random_id.kms_key_ring_suffix.hex}"
   location = var.gcp_region
 }
