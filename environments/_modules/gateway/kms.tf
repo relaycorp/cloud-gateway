81cdab7d04939d98826b2ee53c8a54e590925d87
Gus Narea <gnarea@users.noreply.github.com>
2020-12-06 12:41:55 +0000

    KMS: Move key ring into region

--- environments/_modules/gateway/kms.tf
+++ environments/_modules/gateway/kms.tf
@@ -1,6 +1,10 @@
+resource "random_id" "kms_key_ring_suffix" {
+  byte_length = 3
+}
+
 resource "google_kms_key_ring" "main" {
-  project = var.gcp_project_id
+  project = "${var.gcp_project_id}-${random_id.kms_key_ring_suffix.hex}"
 
   name     = local.env_full_name
-  location = "global"
+  location = var.gcp_region
 }
