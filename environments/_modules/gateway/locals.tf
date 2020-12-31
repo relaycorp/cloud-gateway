c7f755d42176a1638a13e752febe0047c952c7ee
Gus Narea <gnarea@users.noreply.github.com>
2020-12-31 10:00:25 +0000

    GCB: Receive public gateway address

--- environments/_modules/gateway/locals.tf
+++ environments/_modules/gateway/locals.tf
@@ -1,6 +1,10 @@
 locals {
   env_full_name = "gateway-${var.name}"
 
+  gateway = {
+    public_address = "${var.name}.${trimsuffix(data.google_dns_managed_zone.main.dns_name, ".")}"
+  }
+
   gcp_resource_labels = {
     environment = var.name
   }
