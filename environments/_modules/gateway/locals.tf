2a03285eee76a9499c5600456ef99a878280131a
Gus Narea <gnarea@users.noreply.github.com>
2021-01-25 12:59:07 +0000

    Gateway: Use workload identity to authenticate with GCP services
    Needed for #1

--- environments/_modules/gateway/locals.tf
+++ environments/_modules/gateway/locals.tf
@@ -2,8 +2,14 @@ locals {
   env_full_name = "gateway-${var.name}"
 
   gateway = {
+    k8s = {
+      namespace      = "default"
+      serviceAccount = "public-gateway"
+    }
     public_address = "${var.name}.${trimsuffix(data.google_dns_managed_zone.main.dns_name, ".")}"
   }
+
+  workload_identity_pool = "${data.google_project.main.project_id}.svc.id.goog"
 
   gcp_resource_labels = {
     environment = var.name
