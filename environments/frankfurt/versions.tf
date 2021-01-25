0a150d7c0036100e43fd0cdb0d495af8acfc8b40
Gus Narea <gnarea@users.noreply.github.com>
2021-01-25 14:37:35 +0000

    Gateway: Upgrade Google Terraform provider

--- environments/frankfurt/versions.tf
+++ environments/frankfurt/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.27"
+      version = "~> 3.53.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.27"
+      version = "~> 3.53.0"
     }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
