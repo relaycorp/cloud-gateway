d67b19091cff30d2937864b41e81fe2dcb7da397
Gus Narea <gnarea@users.noreply.github.com>
2021-01-26 16:59:46 +0000

    Gateway: Grant myself the role roles/monitoring.admin
    Another attempt to work around https://github.com/hashicorp/terraform-provider-google/issues/2605

--- environments/_modules/gateway/stackdriver.tf
+++ environments/_modules/gateway/stackdriver.tf
@@ -1,6 +1,6 @@
 resource "google_project_iam_binding" "stackdriver_dashboard_editor" {
   // Grant ourselves permission to create workspaces because Terraform doesn't support it:
   // https://github.com/hashicorp/terraform-provider-google/issues/2605
-  role = "roles/monitoring.dashboardEditor"
+  role = "roles/monitoring.admin"
   members = [var.sre_iam_uri]
 }
