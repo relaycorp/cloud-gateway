20d5fd8c103254d2283ec3426c3cd7403710e6b5
Gus Narea <gnarea@users.noreply.github.com>
2021-02-02 18:24:49 +0000

    Gateway: Monitor public services

--- environments/_modules/gateway/monitoring.tf
+++ environments/_modules/gateway/monitoring.tf
@@ -9,3 +9,84 @@ resource "google_project_iam_binding" "error_reporting_sre_access" {
   role    = "roles/errorreporting.user"
   members = [var.sre_iam_uri]
 }
+
+resource "google_monitoring_group" "main" {
+  display_name = local.env_full_name
+
+  filter = "resource.metadata.region=\"${var.gcp_region}\""
+}
+
+resource "google_monitoring_uptime_check_config" "poweb" {
+  display_name = "${local.env_full_name}-poweb"
+  timeout      = "5s"
+  period       = "300"
+
+  http_check {
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.gcp_project_id
+      host       = trimsuffix(google_dns_record_set.poweb.name, ".")
+    }
+  }
+
+  resource_group {
+    resource_type = "INSTANCE"
+    group_id      = google_monitoring_group.main.name
+  }
+}
+
+resource "google_monitoring_uptime_check_config" "cogrpc" {
+  display_name = "${local.env_full_name}-cogrpc"
+  timeout      = "5s"
+  period       = "300"
+
+  http_check {
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.gcp_project_id
+      host       = trimsuffix(google_dns_record_set.cogrpc.name, ".")
+    }
+  }
+
+  resource_group {
+    resource_type = "INSTANCE"
+    group_id      = google_monitoring_group.main.name
+  }
+}
+
+resource "google_monitoring_uptime_check_config" "pohttp" {
+  display_name = "${local.env_full_name}-pohttp"
+  timeout      = "5s"
+  period       = "300"
+
+  http_check {
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.gcp_project_id
+      host       = trimsuffix(google_dns_record_set.pohttp.name, ".")
+    }
+  }
+
+  resource_group {
+    resource_type = "INSTANCE"
+    group_id      = google_monitoring_group.main.name
+  }
+}
