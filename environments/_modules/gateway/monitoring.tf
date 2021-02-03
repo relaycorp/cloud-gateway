d897fd5ae07daf05593bb14ab327f2fab9cbaa3f
Gus Narea <gnarea@users.noreply.github.com>
2021-02-03 18:20:53 +0000

    Gateway: Refactor uptime monitoring config

--- environments/_modules/gateway/monitoring.tf
+++ environments/_modules/gateway/monitoring.tf
@@ -16,89 +16,30 @@ resource "google_monitoring_group" "main" {
   filter = "resource.metadata.region=\"${var.gcp_region}\""
 }
 
-resource "google_monitoring_uptime_check_config" "poweb" {
-  display_name = "${local.env_full_name}-poweb"
-  timeout      = "5s"
-  period       = "300s"
+module "poweb_lb_uptime" {
+  source = "../host_uptime_monitor"
 
-  http_check {
-    port         = "443"
-    use_ssl      = true
-    validate_ssl = true
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = var.gcp_project_id
-      host       = trimsuffix(google_dns_record_set.poweb.name, ".")
-    }
-  }
+  name                 = "${local.env_full_name}-poweb"
+  host_name            = google_dns_record_set.poweb.name
+  notification_channel = data.terraform_remote_state.root.outputs.sre_monitoring_notification_channel
+  gcp_project_id       = var.gcp_project_id
 }
 
-resource "google_monitoring_alert_policy" "poweb_lb_uptime" {
-  display_name = "${local.env_full_name}-poweb-uptime"
-  combiner     = "OR"
-  conditions {
-    display_name = "Uptime health check"
-    condition_threshold {
-      filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" metric.label.\"check_id\"=\"${basename(google_monitoring_uptime_check_config.poweb.id)}\""
-      duration        = "60s"
-      comparison      = "COMPARISON_GT"
-      threshold_value = 1.0
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period     = "300s"
-        cross_series_reducer = "REDUCE_COUNT_FALSE"
-        group_by_fields      = ["resource.*"]
-        per_series_aligner   = "ALIGN_NEXT_OLDER"
-      }
-    }
-  }
+module "pohttp_lb_uptime" {
+  source = "../host_uptime_monitor"
 
-  notification_channels = [
-    data.terraform_remote_state.root.outputs.sre_monitoring_notification_channel
-  ]
-
-  user_labels = local.gcp_resource_labels
+  name                 = "${local.env_full_name}-pohttp"
+  host_name            = google_dns_record_set.pohttp.name
+  notification_channel = data.terraform_remote_state.root.outputs.sre_monitoring_notification_channel
+  gcp_project_id       = var.gcp_project_id
 }
 
-resource "google_monitoring_uptime_check_config" "cogrpc" {
-  display_name = "${local.env_full_name}-cogrpc"
-  timeout      = "5s"
-  period       = "300s"
+module "cogrpc_lb_uptime" {
+  source = "../host_uptime_monitor"
 
-  tcp_check {
-    port = 443
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = var.gcp_project_id
-      host       = trimsuffix(google_dns_record_set.cogrpc.name, ".")
-    }
-  }
-}
-
-resource "google_monitoring_uptime_check_config" "pohttp" {
-  display_name = "${local.env_full_name}-pohttp"
-  timeout      = "5s"
-  period       = "300s"
-
-  http_check {
-    port         = "443"
-    use_ssl      = true
-    validate_ssl = true
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = var.gcp_project_id
-      host       = trimsuffix(google_dns_record_set.pohttp.name, ".")
-    }
-  }
+  name                 = "${local.env_full_name}-cogrpc"
+  probe_type           = "tcp"
+  host_name            = google_dns_record_set.cogrpc.name
+  notification_channel = data.terraform_remote_state.root.outputs.sre_monitoring_notification_channel
+  gcp_project_id       = var.gcp_project_id
 }
---
+++ environments/_modules/host_uptime_monitor/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {}
+
+variable "host_name" {}
+
+variable "probe_type" {
+  default = "https"
+
+  validation {
+    condition     = contains(["https", "tcp"], var.probe_type)
+    error_message = "Probe type should be 'https' or 'tcp'."
+  }
+}
+
+variable "gcp_project_id" {}
+
+variable "notification_channel" {}
