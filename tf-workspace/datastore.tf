071b130bc97de121c57ce6aa276e8d67d32b7086
Gus Narea <gnarea@users.noreply.github.com>
2022-06-30 16:26:07 +0100

    Rename App Engine resource name

--- tf-workspace/datastore.tf
+++ tf-workspace/datastore.tf
@@ -1,4 +1,4 @@
-resource "google_app_engine_application" "app" {
+resource "google_app_engine_application" "main" {
   // We don't actually use App Engine but this is the only way to enable Firebase Datastore
   project       = var.gcp_project_id
   location_id   = "europe-west3" // TODO: This should be environment-specific
