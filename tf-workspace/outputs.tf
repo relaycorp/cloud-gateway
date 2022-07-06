22de4600b4468f0a1134b5ad0f8dacb5e9292210
Gus Narea <gnarea@users.noreply.github.com>
2022-07-06 18:27:34 +0100

    Main workspace: Mark outputs as non-sensitive

--- tf-workspace/outputs.tf
+++ tf-workspace/outputs.tf
@@ -1,11 +1,14 @@
 output "sre_monitoring_notification_channel" {
-  value = var.sre_monitoring_notification_channel
+  value     = var.sre_monitoring_notification_channel
+  sensitive = false
 }
 
 output "sre_email_addresses" {
-  value = var.sre_email_addresses
+  value     = var.sre_email_addresses
+  sensitive = false
 }
 
 output "gcp_parent_folder" {
-  value = var.gcp_parent_folder
+  value     = var.gcp_parent_folder
+  sensitive = false
 }
