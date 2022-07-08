7fc8feb944adb79037fb2a99413e8955f41c80d1
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 21:33:29 +0100

    Grant roles/serviceusage.serviceUsageConsumer to SREs

--- tf-workspace/gcp.tf
+++ tf-workspace/gcp.tf
@@ -6,6 +6,12 @@ resource "google_folder_iam_member" "project_viewer" {
   member = var.sre_iam_uri
 }
 
+resource "google_folder_iam_member" "service_usage_consumer" {
+  folder = var.gcp_parent_folder
+  role   = "roles/serviceusage.serviceUsageConsumer"
+  member = var.sre_iam_uri
+}
+
 resource "google_project_service" "dns" {
   project                    = var.gcp_project_id
   service                    = "dns.googleapis.com"
