c07f90ebad098bfaec30ceb46e7a8faf6bef16d2
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 12:20:23 +0100

    Move GCP resources into env-specific module

--- environments/_modules/gateway/gke.tf
+++ environments/_modules/gateway/gke.tf
@@ -141,3 +141,9 @@ resource "google_project_iam_binding" "gke_limited_admin" {
 
   # TODO: Limit to a single GKE cluster
 }
+
+resource "google_project_iam_binding" "gke_developers" {
+  project = var.gcp_project_id
+  role    = "roles/container.developer"
+  members = [var.sre_iam_uri, "serviceAccount:${local.gcb_service_account_email}"]
+}
