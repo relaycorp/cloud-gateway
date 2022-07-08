4fff4482ee26edd6eb2f4b1584ce14ab83b9f64d
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 16:06:54 +0100

    Revert "Reinstate Frankfurt workspace"
    This reverts commit 013291f96391097302414ca8e4d450a4e5067246.

--- tf-workspace/environments.tf
+++ tf-workspace/environments.tf
@@ -1,18 +1,18 @@
-module "env_frankfurt" {
-  source = "../tf-modules/environment_workspace"
-
-  name = "frankfurt"
-
-  gcp_parent_folder   = var.gcp_parent_folder
-  gcp_billing_account = var.gcp_billing_account
-
-  mongodb_atlas_org_id          = var.mongodb_atlas_org_id
-  env_mongodb_atlas_public_key  = var.env_mongodb_atlas_public_key
-  env_mongodb_atlas_private_key = var.env_mongodb_atlas_private_key
-}
+#module "env_frankfurt" {
+#  source = "../tf-modules/environment_workspace"
+#
+#  name = "frankfurt"
+#
+#  gcp_parent_folder   = var.gcp_parent_folder
+#  gcp_billing_account = var.gcp_billing_account
+#
+#  mongodb_atlas_org_id          = var.mongodb_atlas_org_id
+#  env_mongodb_atlas_public_key  = var.env_mongodb_atlas_public_key
+#  env_mongodb_atlas_private_key = var.env_mongodb_atlas_private_key
+#}
 
 locals {
   env_workspace_ids = [
-    module.env_frankfurt.tfe_workspace_id,
+    #    module.env_frankfurt.tfe_workspace_id,
   ]
 }
