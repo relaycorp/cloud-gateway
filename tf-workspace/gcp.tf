901db526d4e192b9e3ca673e2e62190729899109
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 16:20:56 +0100

    Reenable GCP services serviceusage and cloudresourcemanager

--- tf-workspace/gcp.tf
+++ tf-workspace/gcp.tf
@@ -12,3 +12,15 @@ resource "google_project_service" "dns" {
   service                    = "dns.googleapis.com"
   disable_dependent_services = true
 }
+
+resource "google_project_service" "serviceusage" {
+  project                    = var.gcp_project_id
+  service                    = "serviceusage.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  project                    = var.gcp_project_id
+  service                    = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
+}
