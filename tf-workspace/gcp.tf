159580ca2d63bb5796d4c340a3a00e3384931bed
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 16:51:15 +0100

    Grant roles/viewer to SREs

--- tf-workspace/gcp.tf
+++ tf-workspace/gcp.tf
@@ -1,14 +1,14 @@
 data "google_project" "main" {
 }
 
-resource "google_project_iam_member" "project_viewer" {
-  project = var.gcp_project_id
-  role    = "roles/viewer"
-  member  = var.sre_iam_uri
-}
-
 resource "google_project_service" "dns" {
   project                    = var.gcp_project_id
   service                    = "dns.googleapis.com"
   disable_dependent_services = true
+}
+
+resource "google_folder_iam_member" "project_viewer" {
+  folder = var.gcp_parent_folder
+  role   = "roles/viewer"
+  member = var.sre_iam_uri
 }
