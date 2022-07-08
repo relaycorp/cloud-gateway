06aece4905c4a892dd929ba46162afafa3e37c82
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 17:21:14 +0100

    Set GCP project id explicitly in DNS zone

--- tf-workspace/dns.tf
+++ tf-workspace/dns.tf
@@ -1,4 +1,5 @@
 resource "google_dns_managed_zone" "relaycorp_cloud" {
+  project     = var.gcp_project_id
   name        = "relaycorp-cloud"
   dns_name    = "relaycorp.cloud."
   description = "Relaycorp Cloud"
