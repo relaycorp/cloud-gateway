e89b1719d44cd5f2f33949f838b70d64b6e9448c
Gus Narea <gnarea@users.noreply.github.com>
2022-07-08 18:00:23 +0100

    Temporarily disable frankfurt workspace

--- tf-workspace/environments.tf
+++ tf-workspace/environments.tf
@@ -1,20 +1,18 @@
-module "env_frankfurt" {
-  source = "../tf-modules/environment_workspace"
-
-  name = "frankfurt"
-
-  gcp_parent_folder   = var.gcp_parent_folder
-  gcp_billing_account = var.gcp_billing_account
-
-  mongodb_atlas_org_id          = var.mongodb_atlas_org_id
-  env_mongodb_atlas_public_key  = var.env_mongodb_atlas_public_key
-  env_mongodb_atlas_private_key = var.env_mongodb_atlas_private_key
-
-  depends_on = [google_project_service.iam]
-}
+#module "env_frankfurt" {
+#  source = "../tf-modules/environment_workspace"
+#
+#  name = "frankfurt"
+#
+#  gcp_parent_folder   = var.gcp_parent_folder
+#  gcp_billing_account = var.gcp_billing_account
+#
+#  mongodb_atlas_org_id          = var.mongodb_atlas_org_id
+#  env_mongodb_atlas_public_key  = var.env_mongodb_atlas_public_key
+#  env_mongodb_atlas_private_key = var.env_mongodb_atlas_private_key
+#}
 
 locals {
   env_workspace_ids = [
-    module.env_frankfurt.tfe_workspace_id,
+    #    module.env_frankfurt.tfe_workspace_id,
   ]
 }
