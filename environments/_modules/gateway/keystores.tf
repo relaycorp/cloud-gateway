58dcc8c49f8666b372bc58f718c69f69db547e86
Gus Narea <gnarea@users.noreply.github.com>
2023-02-01 10:31:09 +0000

    Revert "Temporarily grant SREs permission to impersonate app SA (#91)"
    This reverts commit 3c9af3b9

--- environments/_modules/gateway/keystores.tf
+++ environments/_modules/gateway/keystores.tf
@@ -87,10 +87,3 @@ resource "google_project_iam_member" "keystore_kms_user" {
     expression = "resource.name.startsWith(\"${google_kms_key_ring.keystores.id}\")"
   }
 }
-
-// TODO: Remove when https://console.cloud.google.com/errors/detail/COW11ovZ2aG8NQ;time=PT6H?project=gw-frankfurt-4065
-// is fixed
-resource "google_project_iam_member" "keystore_kms_admin_user_tmp" {
-  role   = "roles/iam.serviceAccountKeyAdmin"
-  member  = var.sre_iam_uri
-}
