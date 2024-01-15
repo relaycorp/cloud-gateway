ce16774b6eac86b9ecb79fe540f5e6b32997829b
Gus Narea <gnarea@users.noreply.github.com>
2024-01-15 15:20:49 +0000

    Create DNS zone relaycorp.services

--- tf-workspace/dns.tf
+++ tf-workspace/dns.tf
@@ -10,3 +10,16 @@ resource "google_dns_managed_zone" "relaycorp_cloud" {
 
   depends_on = [google_project_service.dns]
 }
+
+resource "google_dns_managed_zone" "relaycorp_services" {
+  project     = var.gcp_project_id
+  name        = "relaycorp-services"
+  dns_name    = "relaycorp.services."
+  description = "Relaycorp Cloud"
+
+  dnssec_config {
+    state = "on"
+  }
+
+  depends_on = [google_project_service.dns]
+}
