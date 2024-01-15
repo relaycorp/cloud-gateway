59a60c06e2a156b84176ad7622e0290b3d18f348
Gus Narea <gnarea@users.noreply.github.com>
2024-01-15 16:56:55 +0000

    Belgium: define TFE var `alert_email_addresses`

--- tf-workspace/tfe.tf
+++ tf-workspace/tfe.tf
@@ -45,6 +45,16 @@ resource "tfe_variable" "sre_email" {
   sensitive = false
 }
 
+resource "tfe_variable" "alert_email_addresses" {
+  variable_set_id = tfe_variable_set.environments.id
+
+  category  = "terraform"
+  key       = "alert_email_addresses"
+  value     = jsonencode(var.sre_email_addresses)
+  hcl       = true
+  sensitive = false
+}
+
 resource "tfe_variable" "gcp_shared_infra_project_id" {
   variable_set_id = tfe_variable_set.environments.id
 
