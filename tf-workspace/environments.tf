febebcd09f6deaa8ca49882e26980b585452a44e
Gus Narea <gnarea@users.noreply.github.com>
2024-01-18 21:54:40 +0000

    Frankfurt: Remove workspace

--- tf-workspace/environments.tf
+++ tf-workspace/environments.tf
@@ -1,14 +1,3 @@
-module "env_frankfurt" {
-  source = "../tf-modules/environment_workspace"
-
-  name = "frankfurt"
-
-  gcp_parent_folder   = var.gcp_parent_folder
-  gcp_billing_account = var.gcp_billing_account
-
-  shared_infra_gcp_project_id = data.google_project.main.project_id
-}
-
 module "env_belgium" {
   source = "../tf-modules/serverless_environment_workspace"
 
@@ -25,7 +14,6 @@ module "env_belgium" {
 
 locals {
   env_workspace_ids = [
-    module.env_frankfurt.tfe_workspace_id,
     module.env_belgium.tfe_workspace_id,
   ]
 }
